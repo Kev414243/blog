3d521f81d8ae2b931d2adf8322854ead5954740e
kev414243 <kevin_galvez4142@hotmail.com>
2024-08-22 22:55:11 -0600

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,87 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :check_post_exists, only: %i[update destroy]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  rescue ActiveRecord::RecordNotDestroyed
+    respond_to do |format|
+      format.html { redirect_to posts_url, alert: "Post could not be destroyed." }
+      format.json { render json: { error: "Post could not be destroyed" }, status: :unprocessable_entity }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      /@post = Post.find(params[:id])/
+      @post = Post.find_by(id: params[:id])
+    end
+
+    # Ensure the post exists before update or destroy actions
+    def check_post_exists
+      unless @post
+        respond_to do |format|
+          format.html { redirect_to posts_url, alert: "Post not found." }
+          format.json { render json: { error: "Post not found" }, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
